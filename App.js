c4430085eac1964ff02290c1f8bd4413b77c6986
Carolina Pirela <caropi30@gmail.com>
2022-08-31 20:49:06 -0400

    construyendo un input

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>HOLAAAA, CODER!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginHorizontal: 20,
+  },
+  inputContainer: {
+    marginTop: 50,
+    marginBottom: 20,
+    color: "#212121",
+    height: 40,
+    borderBottomColor: "#4a306d",
+  },
+  textInput: {
+    borderColor: "#212121",
   },
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <TextInput
+        styles={styles.textInput}
+        placeholder="Escribe una tarea"
+        selectionColor="#4a306d"
+      />
+      <Button title="Add" onPress={() => console.warn("click")} />
+    </View>
+  );
+}
